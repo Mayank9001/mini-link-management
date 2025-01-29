7fad70a5394f4e89be5c8902c3d1b6a32d683cab
Mayank Vishwakarma <mayankvishw09@gmail.com>
2025-01-29 23:29:57 +0530

    feat: dashboard done

--- frontend/src/pages/Dashboard.jsx
+++ frontend/src/pages/Dashboard.jsx
@@ -8,8 +8,8 @@ import styles from './Dashboard.module.css'
 const Dashboard = () => {
   const [user, setUser] = useState();
   const [totalClicks, setTotalClicls] = useState();
-  const [deviceTypeClicks, setDeviceTypeClicks] = useState();
-  const [dateWiseClicks, setDateWiseClicks] = useState();
+  const [deviceTypeClicks, setDeviceTypeClicks] = useState([]);
+  const [dateWiseClicks, setDateWiseClicks] = useState([]);
   const isActive = {
     dashboard :true,
     links :false,
@@ -41,6 +41,12 @@ const Dashboard = () => {
       console.log(error);
     }
   };
+  const sortedDateWiseClicks = dateWiseClicks.length > 0 ? [...dateWiseClicks].sort((a, b) => {
+    const dateA = new Date(a._id.split("-").reverse().join("-"));
+    const dateB = new Date(b._id.split("-").reverse().join("-"));
+    return dateB - dateA;
+  }) : [];
+  const sortedDeviceClicks = deviceTypeClicks.length > 0 ? [...deviceTypeClicks].sort((a, b) => { return b.clicks - a.clicks}) : [];
 
   useEffect(()=>{
     getuser();
@@ -51,19 +57,56 @@ const Dashboard = () => {
   },[user]);
   // console.log("Data", data);
   // console.log("TC", totalClicks);
-  // console.log("Date", dateWiseClicks);
-  // console.log("Device", deviceTypeClicks);
+  // console.log("Date", sortedDateWiseClicks);
+  // console.log("Device", sortedDeviceClicks);
   return (
     <>
       <Navbar />  
       <SideBar isDashboard={isActive.dashboard} isSettings={isActive.settings} isLinks={isActive.links} isAnalytics={isActive.analytics} />
       <div className={styles.main}>
-        <div>
-          <span>Total clicks : {totalClicks? totalClicks : "0"}</span>
+        <div className={styles.clicks}>
+          Total Clicks
+          <span>{totalClicks? totalClicks : "0"}</span>
         </div>
-        <div>
-          <div>Date-wise Clicks</div>
-          <div>Device Clicks</div>
+        <div className={styles.chart}>
+          <div className={styles.boxes}>
+            <span className={styles.title}>
+              Date-wise Clicks
+            </span>
+            <div className={styles.chartcontainer}>
+              {sortedDateWiseClicks.map(({ _id, clicks }) => (
+                <div key={_id} className={styles.chartrow}>
+                  <span className={styles.datelabel}>{_id}</span>
+                  <div className={styles.barcontainer}>
+                    <div
+                      className={styles.bar}
+                      style={{ width: `${(clicks / totalClicks) * 100}%` }}
+                    ></div>
+                  </div>
+                  <span className={styles.clickcount}>{clicks}</span>
+                </div>
+              ))}
+            </div>
+          </div>
+          <div className={styles.boxes}>
+            <span className={styles.title}>
+                Click Devices
+            </span>
+            <div className={styles.chartcontainer}>
+              {sortedDeviceClicks.map(({ _id, clicks }) => (
+                <div key={_id} className={styles.chartrow}>
+                  <span className={styles.devicelabel}>{_id}</span>
+                  <div className={styles.barcontainer}>
+                    <div
+                      className={styles.bar}
+                      style={{ width: `${(clicks / totalClicks) * 100}%` }}
+                    ></div>
+                  </div>
+                  <span className={styles.clickcount}>{clicks}</span>
+                </div>
+              ))}
+            </div>
+          </div>
         </div>
       </div>
     </>
